a3ca559c23ea96e80e1e5e3a56815f63e86cf5ce
Bishowjit <77982152+Bishow-jit@users.noreply.github.com>
2024-07-30 09:50:00 +0600

    Initial commit

---
+++ src/test/java/com/example/ProjectManagementSystem/ProjectManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
