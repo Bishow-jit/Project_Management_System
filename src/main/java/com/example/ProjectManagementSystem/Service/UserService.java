9102c44d905298e594139561131434ef964eb2c8
Bishowjit-Saha <77982152+Bishow-jit@users.noreply.github.com>
2024-08-06 14:23:39 +0600

    users get

--- src/main/java/com/example/ProjectManagementSystem/Service/UserService.java
+++ src/main/java/com/example/ProjectManagementSystem/Service/UserService.java
@@ -3,8 +3,15 @@ package com.example.ProjectManagementSystem.Service;
 import com.example.ProjectManagementSystem.Entity.Users;
 import com.example.ProjectManagementSystem.Repository.Usersrepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -28,5 +35,18 @@ public class UserService {
             return "Registration Failed";
         }
 
+    }
+
+    public ResponseEntity<?> getAllUsersWithoutLoggedInUser(Principal principal) {
+        try {
+            String loggedInUser = principal.getName();
+            List<Users> usersList = usersrepository.findAllByActiveTrue();
+            List<Users> filteredUsersList = usersList.stream()
+                    .filter(user -> !Objects.equals(user.getUsername(), loggedInUser)).toList();
+            return ResponseEntity.ok(filteredUsersList);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while getting Users");
+        }
     }
 }
