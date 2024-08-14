85e6683e89f17bfef7448bd94526433721ab5566
Bishowjit <77982152+Bishow-jit@users.noreply.github.com>
2024-08-14 10:49:59 +0600

    swagger description updated

--- src/main/java/com/example/ProjectManagementSystem/Configuration/SwaggerConfig.java
+++ src/main/java/com/example/ProjectManagementSystem/Configuration/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
                 .components(new Components().addSecuritySchemes
                         ("Bearer Authentication", createAPIKeyScheme()))
                 .info(new Info().title("PMS API")
-                        .description("API of Project Management System.")
+                        .description("Backend API Of Project Management System.")
                         .version("1.0").contact(new Contact().name("Bishowjit")));
     }
 
